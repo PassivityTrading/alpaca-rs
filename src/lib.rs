aa6c8c862de871bad863eca5d727ff10add7798d
nothendev <borodinov.ilya@gmail.com>
2023-11-29 19:57:25 +0300

    add standalone trading client & account view

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
-use std::{future::Future, pin::Pin};
+use std::{future::Future, ops::Deref, pin::Pin};
 
 use base64::Engine;
-use reqwest::{header::AUTHORIZATION, Method, Url};
+use model::Account;
+use reqwest::{
+    header::{HeaderMap, AUTHORIZATION},
+    Method, Url,
+};
 use serde::de::DeserializeOwned;
 
 pub mod model;
 
 pub struct BrokerAuth {
-    pub key: String,
+    pub key: Vec<u8>,
 }
 
 pub struct BrokerClient {
     pub reqwest: reqwest::Client,
-    pub broker_base_url: Url,
+    pub base_url: Url,
     auth: BrokerAuth,
 }
 
@@ -25,7 +29,7 @@ impl BrokerClient {
     pub fn new_prod(auth: BrokerAuth) -> Self {
         Self {
             reqwest: reqwest::Client::new(),
-            broker_base_url: Url::parse(BROKER_PROD).unwrap(),
+            base_url: Url::parse(BROKER_PROD).unwrap(),
             auth,
         }
     }
@@ -33,9 +37,17 @@ impl BrokerClient {
     pub fn new_sandbox(auth: BrokerAuth) -> Self {
         Self {
             reqwest: reqwest::Client::new(),
-            broker_base_url: Url::parse(BROKER_SANDBOX).unwrap(),
+            base_url: Url::parse(BROKER_SANDBOX).unwrap(),
             auth,
         }
+    }
+
+    pub fn with_reqwest(self, reqwest: reqwest::Client) -> Self {
+        Self { reqwest, ..self }
+    }
+
+    pub fn with_base_url(self, base_url: Url) -> Self {
+        Self { base_url, ..self }
     }
 
     fn br_auth(&self) -> String {
@@ -74,6 +86,43 @@ impl BrokerClient {
             .header(AUTHORIZATION, self.br_auth());
         T::deserialize(request.send().await?).await
     }
+
+    pub async fn account(&self, id: &str) -> Result<AccountView<'_>> {
+        Ok(AccountView {
+            data: self.execute_trading(model::trading::GetAccount, id).await?,
+            client: self,
+        })
+    }
+}
+
+pub struct AccountView<'a> {
+    pub data: Account,
+    client: &'a BrokerClient,
+}
+
+impl<'a> Deref for AccountView<'a> {
+    type Target = Account;
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<'a> AccountView<'a> {
+    pub async fn refetch(&mut self) -> Result<()> {
+        self.data = self
+            .client
+            .execute_trading(model::trading::GetAccount, &self.data.id)
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn execute<T: Endpoint + BrokerTradingEndpoint>(
+        &self,
+        endpoint: T,
+    ) -> Result<T::Result> {
+        self.client.execute_trading(endpoint, &self.data.id).await
+    }
 }
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
@@ -103,28 +152,60 @@ pub trait Endpoint {
 
 pub trait BrokerEndpoint: Endpoint {
     fn base_url(&self, client: &BrokerClient) -> Url {
-        client.broker_base_url.clone()
+        client.base_url.clone()
     }
 }
 
-#[doc(hidden)]
-pub trait Urls {
-    fn broker_base_url(&self) -> Url;
-    fn trading_base_url(&self) -> Url;
+pub struct TradingAuth {
+    pub key: String,
+    pub secret: String,
 }
 
-impl Urls for BrokerClient {
-    fn broker_base_url(&self) -> Url {
-        self.broker_base_url.clone()
+pub struct TradingClient {
+    pub reqwest: reqwest::Client,
+    pub base_url: Url,
+    auth: TradingAuth,
+}
+
+impl TradingClient {
+    pub fn new(auth: TradingAuth) -> Self {
+        Self {
+            reqwest: reqwest::Client::new(),
+            base_url: TRADING_PROD.parse().unwrap(),
+            auth,
+        }
     }
-    fn trading_base_url(&self) -> Url {
-        self.trading_base_url.clone()
+
+    pub fn new_paper(auth: TradingAuth) -> Self {
+        Self {
+            reqwest: reqwest::Client::new(),
+            base_url: TRADING_PAPER.parse().unwrap(),
+            auth,
+        }
+    }
+
+    fn auth_headers(&self) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        let _ = headers.insert("APCA-API-KEY-ID", self.auth.key.parse().unwrap());
+        let _ = headers.insert("APCA-API-SECRET-KEY", self.auth.secret.parse().unwrap());
+        headers
+    }
+
+    pub async fn execute<T: TradingEndpoint>(&self, endpoint: T) -> Result<T::Result> {
+        let request = endpoint
+            .configure(self.reqwest.request(
+                endpoint.method(),
+                endpoint.base_url(self).join(endpoint.url()).unwrap(),
+            ))
+            .headers(self.auth_headers());
+
+        T::deserialize(request.send().await?).await
     }
 }
 
 pub trait TradingEndpoint: Endpoint {
-    fn base_url(&self, client: &impl Urls) -> Url {
-        client.trading_base_url()
+    fn base_url(&self, client: &TradingClient) -> Url {
+        client.base_url.clone()
     }
 }
 
--- src/model/trading.rs
+++ src/model/trading.rs
@@ -1,5 +1,6 @@
 use super::*;
 
+/// Get account details.
 pub struct GetAccount;
 
 impl Endpoint for GetAccount {
@@ -29,6 +30,7 @@ impl BrokerTradingEndpoint for GetAccount {
     }
 }
 
+/// Order amount (number of shares or dollar amount).
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
 pub enum OrderAmount {
@@ -40,19 +42,27 @@ pub enum OrderAmount {
     Notional(#[serde_as(as = "DisplayFromStr")] f64),
 }
 
+/// Create an order.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CreateOrder {
+    /// The symbol/ticker of the stock being traded.
     pub symbol: String,
+    /// Either the quantity or the dollar amount to trade.
     #[serde(flatten)]
     pub amount: OrderAmount,
+    /// Buy or sell.
     pub side: OrderSide,
-    #[serde(rename = "type")]
-    #[serde(flatten)]
+    /// Order type. Includes market, limit, stop, etc. orders.
+    #[serde(flatten, rename = "type")]
     pub kind: OrderType,
+    // TODO explain
     pub time_in_force: OrderTif,
+    /// Specifies if the order is allowed to be processed in extended hours.
     pub extended_hours: bool,
+    // TODO explain
     #[serde(skip_serializing_if = "Option::is_none")]
     pub client_order_id: Option<String>,
+    // TODO explain
     pub order_class: OrderClass,
 }
 
@@ -76,33 +86,47 @@ impl Endpoint for CreateOrder {
 
 impl TradingEndpoint for CreateOrder {}
 
+/// Create an order on behalf of an account in the Broker API.
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CreateOrderBroker {
+    /// The symbol/ticker of the stock being traded.
     pub symbol: String,
+    /// Either the quantity or the dollar amount to trade.
     #[serde(flatten)]
     pub amount: OrderAmount,
+    /// Buy or sell.
     pub side: OrderSide,
-    #[serde(rename = "type")]
-    #[serde(flatten)]
+    /// Order type. Includes market, limit, stop, etc. orders.
+    #[serde(rename = "type", flatten)]
     pub kind: OrderType,
+    // TODO(filter) explain
     pub time_in_force: OrderTif,
+    /// Specifies if the order is allowed to be processed in extended hours.
     pub extended_hours: bool,
+    // TODO explain
     #[serde(skip_serializing_if = "Option::is_none")]
     pub client_order_id: Option<String>,
+    // TODO explain
     pub order_class: OrderClass,
+    /// The commission that the broker takes.
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde_as(as = "Option<serde_with::DisplayFromStr>")]
     pub commission: Option<f64>,
+    // TODO explain
     #[serde(skip_serializing_if = "Option::is_none")]
     #[serde_as(as = "Option<serde_with::DisplayFromStr>")]
     pub commission_bps: Option<f64>,
+    // TODO explain [no official explanation]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub source: Option<String>,
+    // TODO explain [no official explanation]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub instructions: Option<String>,
+    // TODO explain [no official explanation]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub subtag: Option<String>,
+    // TODO explain [no official explanation]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub swap_fee_bps: Option<String>,
 }
